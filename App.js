c6081d65d070b8524b72388320182e75f66422a4
Shubham Srivastava <smjackson94@gmail.com>
2022-02-07 13:04:21 +0530

    new proj

--- App.js
+++ App.js
@@ -1,17 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Fiboview from 'fibo-react';
+import Fibotalk from 'fibo-react-native';
 
 export default function App() {
+  let fibo = new Fibotalk("sample");
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Fiboview
-        appid="d89404ba878c4f874a0171da"
-        ref={(e)=>{fibo = e}}
-      />
     </View>
   );
 }
